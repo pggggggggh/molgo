11eb0d920fe5a7324bbc26024e9f38e5e82db201
pggggggggh <pggggggggh@yonsei.ac.kr>
2024-09-08 03:46:12 +0900

    init

--- src/pages/RatingPage.jsx
+++ src/pages/RatingPage.jsx
@@ -1,5 +1,19 @@
-const RatingPage = () => {
+import RatingChartComponent from "@/components/RatingChartComponent.jsx";
+import {Box, Typography} from "@mui/material";
 
+const RatingPage = () => {
+    return (
+        <Box
+            display={"flex"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            flexDirection={"column"}
+            mt={20}
+        >
+            <Typography variant={"h5"} fontWeight={"bold"}>Codeforces</Typography>
+            <RatingChartComponent/>
+        </Box>
+    );
 };
 
 export default RatingPage;
