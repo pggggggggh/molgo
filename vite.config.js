39ba4d3b7f593f5c5bd53ead697b99829bf530b2
pggggggggh <pggggggggh@yonsei.ac.kr>
2024-09-11 11:32:58 +0900

    favicon

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "node:path";
+import ViteFaviconsPlugin from "vite-plugin-favicon";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+      react(),
+      // ViteFaviconsPlugin({
+      //     logo:"public/assets/favicon.png",})
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
